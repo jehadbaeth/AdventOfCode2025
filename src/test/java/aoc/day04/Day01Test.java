2f43eccf4ef2d3ab9a9ac1efef2848bf60f78efe
jehadbaeth <60194206+jehadbaeth@users.noreply.github.com>
2025-12-01 17:51:18 +0100

    template preparation for tests

---
+++ src/test/java/aoc/day04/Day01Test.java
@@ -0,0 +1,34 @@
+package aoc.day04;
+
+
+import aoc.day01.Day01;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day01Test {
+
+    @Test
+    public void testPart1(){
+        // Given
+        String input = "test";
+
+        // When
+        String result = new Day01().part1(input);
+
+        // Then
+        assertEquals(input, result);
+    }
+
+    @Test
+    public void testPart2(){
+        // Given
+        String input = "test";
+
+        // When
+        String result = new Day01().part2(input);
+
+        // Then
+        assertEquals(input, result);
+    }
+}
